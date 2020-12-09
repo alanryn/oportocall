725162fe336b8cb01f12eec292315eea885cc4e9
Alan Ryan <al.limerick@gmail.com>
2020-12-09 18:57:17 +0000

    adding gsap

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -8,4 +8,41 @@ if (window.netlifyIdentity) {
   });
 }
 
+
+
+
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+barba.use(barbaCss);
+
+const body = document.querySelector('body');
+
+barba.hooks.before((data) => {
+
+  const background = data.current.container.dataset.background;
+  body.style.setProperty('--page-background', background);
+
+});
+
+barba.init({
+  transitions: [
+    {
+      name: 'base',
+      to: {
+        namespace: ['base'],
+      },
+      sync: true,
+      once() {},
+      leave() {},
+      enter() {},
+    },
+   
+  ],
+});
  
+ 
+
+
+
+
