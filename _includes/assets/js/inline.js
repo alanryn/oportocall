55ef23aa640470410e24426216828594f084d8e9
Alan Ryan <al.limerick@gmail.com>
2020-12-11 15:56:16 +0000

    barba tranitions

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -11,35 +11,7 @@ if (window.netlifyIdentity) {
 
 
 
-import barba from '@barba/core';
-import barbaCss from '@barba/css';
 
-barba.use(barbaCss);
-
-const body = document.querySelector('body');
-
-barba.hooks.before((data) => {
-
-  const background = data.current.container.dataset.background;
-  body.style.setProperty('--page-background', background);
-
-});
-
-barba.init({
-  transitions: [
-    {
-      name: 'base',
-      to: {
-        namespace: ['base'],
-      },
-      sync: true,
-      once() {},
-      leave() {},
-      enter() {},
-    },
-   
-  ],
-});
  
  
 
